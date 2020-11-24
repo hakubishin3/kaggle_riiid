d23f985cdf4cfba4a950b5961a5cdc188287a918
hakubishin3 <shu@wantedly.com>
2020-11-24 19:40:05 +0000

    Add features

--- features/user_cumulative_user_interactions_by_part.py
+++ features/user_cumulative_user_interactions_by_part.py
@@ -12,22 +12,66 @@ class UserCumulativeUserInteractionsByPart(BaseFeature):
 
     def _read_features_from_bigquery(self) -> pd.DataFrame:
         query = """
-          WITH
-          cumulative AS (
+            WITH
+            aggregation_by_content AS (
+              SELECT
+                content_id,
+                AVG(answered_correctly) AS accuracy
+              FROM
+                `wantedly-individual-shu.riiid.train_questions`
+              WHERE
+                val = 0
+              GROUP BY
+                content_id
+            ),
+            aggregation_by_part AS (
+              SELECT
+                part,
+                AVG(answered_correctly) AS accuracy
+              FROM
+                `wantedly-individual-shu.riiid.train_questions`
+              WHERE
+                val = 0
+              GROUP BY
+                part
+            ),
+            cumulative AS (
+              SELECT
+                row_id,
+                content_id,
+                part,
+                -- leakを防ぐために, 過去レコードから現時点の1つ前のレコードまでを計算範囲とする
+              SUM(1) OVER (PARTITION BY user_id, part ORDER BY timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS cumlative_user_appearance,
+              SUM(answered_correctly) OVER (PARTITION BY user_id, part ORDER BY timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS cumlative_user_corrected_answers,
+              FROM
+                `wantedly-individual-shu.riiid.train_questions`
+            ),
+            summary AS (
+              SELECT
+                cumulative.row_id,
+                cumlative_user_appearance,
+                cumlative_user_corrected_answers,
+                cumlative_user_corrected_answers / cumlative_user_appearance AS mean_user_accuracy,
+                aggregation_by_content.accuracy AS mean_content_accuracy,
+                aggregation_by_part.accuracy AS mean_part_accuracy,
+              FROM
+                cumulative
+              LEFT OUTER JOIN
+                aggregation_by_content
+                ON cumulative.content_id = aggregation_by_content.content_id
+              LEFT OUTER JOIN
+                aggregation_by_part
+                ON cumulative.part = aggregation_by_part.part
+            )
             SELECT
-              row_id,
-              -- leakを防ぐために, 過去レコードから現時点の1つ前のレコードまでを計算範囲とする
-              SUM(1) OVER (PARTITION BY user_id, part ORDER BY timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS cumlative_appearance_per_user,
-              SUM(answered_correctly) OVER (PARTITION BY user_id, part ORDER BY timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS cumlative_corrected_answers_per_user,
+              cumlative_user_appearance,
+              cumlative_user_corrected_answers,
+              mean_user_accuracy,
+              2 * (mean_user_accuracy *  mean_content_accuracy) / (mean_user_accuracy + mean_content_accuracy) AS hmean_user_content_accuracy,
+              2 * (mean_user_accuracy *  mean_part_accuracy) / (mean_user_accuracy + mean_part_accuracy) AS hmean_user_part_accuracy,
+              3 * (mean_user_accuracy *  mean_content_accuracy * mean_part_accuracy) / (mean_user_accuracy * mean_content_accuracy + mean_user_accuracy * mean_part_accuracy + mean_part_accuracy * mean_content_accuracy) AS hmean_user_part_content_accuracy,
             FROM
-              `wantedly-individual-shu.riiid.train_questions`
-          )
-          SELECT
-            cumlative_appearance_per_user,
-            cumlative_corrected_answers_per_user,
-            cumlative_corrected_answers_per_user / cumlative_appearance_per_user AS average_corrected_answers_per_user,
-          FROM
-            cumulative
+              summary
         """
         query += " order by row_id"
         if self.debugging:
