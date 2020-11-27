b1f3bc5e0597c686a80df2d9a76219c5db2f8d3b
hakubishin3 <shu@wantedly.com>
2020-11-27 07:42:10 +0000

    Add fe

--- features/user_cumulative_user_interactions.py
+++ features/user_cumulative_user_interactions.py
@@ -12,22 +12,66 @@ class UserCumulativeUserInteractions(BaseFeature):
 
     def _read_features_from_bigquery(self) -> pd.DataFrame:
         query = """
-            WITH
-            cumulative AS (
-              SELECT
-                row_id,
-                -- leakを防ぐために, 過去レコードから現時点の1つ前のレコードまでを計算範囲とする
+          WITH
+          global_mean AS (
+            SELECT
+              AVG(answered_correctly) AS mean,
+            FROM
+              `wantedly-individual-shu.riiid.train_questions`
+            WHERE
+              val = 0   -- use only train
+          ),
+          train AS (
+            SELECT
+              content_id,
+              val,
+              answered_correctly,
+              row_number() over(partition by user_id, content_id order by timestamp) as rank
+            FROM
+              `wantedly-individual-shu.riiid.train_questions`
+          ),
+          aggregation_per_content AS (
+            SELECT
+              content_id,
+              AVG(answered_correctly) AS accuracy
+            FROM
+              train
+            WHERE
+              val = 0   -- use only train
+              AND rank = 1   -- 各ユーザの1回目の正解データのみ使う
+            GROUP BY
+              content_id
+          ),
+          cumulative AS (
+            SELECT
+              row_id,
+              content_id,
+              -- leakを防ぐために, 過去レコードから現時点の1つ前のレコードまでを計算範囲とする
                 SUM(1) OVER (PARTITION BY user_id ORDER BY timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS cumlative_user_appearance,
                 SUM(answered_correctly) OVER (PARTITION BY user_id ORDER BY timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS cumlative_user_corrected_answers,
-              FROM
-                `wantedly-individual-shu.riiid.train_questions`
-            )
+            FROM
+              `wantedly-individual-shu.riiid.train_questions`
+          ),
+          summary AS (
             SELECT
+              row_id,
+              content_id,
               cumlative_user_appearance,
               cumlative_user_corrected_answers,
-              cumlative_user_corrected_answers / cumlative_user_appearance AS mean_user_accuracy,
+              (cumlative_user_corrected_answers + 20 * (SELECT mean FROM global_mean))/ (cumlative_user_appearance + 20) AS mean_user_accuracy,
             FROM
               cumulative
+          )
+          SELECT
+            summary.cumlative_user_appearance,
+            summary.cumlative_user_corrected_answers,
+            summary.mean_user_accuracy,
+            2 * (aggregation_per_content.accuracy *  summary.mean_user_accuracy) / (aggregation_per_content.accuracy + summary.mean_user_accuracy) AS hmean_user_content_accuracy,
+          FROM
+            summary
+          LEFT OUTER JOIN
+            aggregation_per_content
+            ON summary.content_id = aggregation_per_content.content_id
         """
         query += " order by row_id"
         if self.debugging:
